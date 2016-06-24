3768dfe5b1209e9a7516347a8af4e58e8992d4ef
Ed-McD <edard_666@hotmail.co.uk>
2016-06-24 20:42:09 +0100

    Player Movement with jumping

---
+++ Assets/GroundCheck.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundCheck : MonoBehaviour {
+
+    [SerializeField]
+    Player_Movement player;
+
+	// Use this for initialization
+	void Awake ()
+    {
+	    if (player == null)
+        {
+            player = GetComponentInParent<Player_Movement>();
+        }
+	}    
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag != "Player")
+        {
+            player.SetGrounded(true);
+        }
+    }
+}
