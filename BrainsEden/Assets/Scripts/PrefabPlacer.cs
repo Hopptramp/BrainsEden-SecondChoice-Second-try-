59558eb9c07619d3bea8355e2ba72450f844d258
Hopptramp <rob-hopps@hotmail.co.uk>
2016-06-24 19:50:27 +0100

    Prefab placer

---
+++ BrainsEden/Assets/Scripts/PrefabPlacer.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabPlacer : MonoBehaviour
+{
+    float addPosX = 0;
+    float addPosY = 0;
+    float addPosZ = 0;
+
+    [SerializeField] GameObject prefabToPlace;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        addPosX = 0;
+        addPosY = 0;
+        addPosZ = 0;
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ++addPosZ;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            --addPosZ;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            --addPosX;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            ++addPosX;
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ++addPosY;
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            --addPosY;
+        }
+
+        transform.position += new Vector3(addPosX, addPosY, addPosZ);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(prefabToPlace, transform.position, Quaternion.identity);
+        }
+    }
+}
