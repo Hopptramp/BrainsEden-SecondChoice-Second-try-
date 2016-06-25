4cc5b71320ea7808e8bf180a2305a39f8a86aed3
Hopptramp <rob-hopps@hotmail.co.uk>
2016-06-25 16:30:43 +0100

    Menus

---
+++ BrainsEden/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+   [SerializeField] GameObject mainMenu;
+   [SerializeField] GameObject optionsMenu;
+    GameObject activeMenu;
+
+
+	public void ResumeGame()
+    {
+        //tween down
+        gameObject.SetActive(false);
+    }
+
+    public void Options()
+    {
+        optionsMenu.SetActive(true);
+        activeMenu.SetActive(false);
+        activeMenu = optionsMenu;
+    }
+
+    public void Back()
+    {
+        mainMenu.SetActive(true);
+        activeMenu.SetActive(false);
+        activeMenu = optionsMenu;
+    }
+
+    public void Exit()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
