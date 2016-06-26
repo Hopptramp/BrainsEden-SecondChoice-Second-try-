71387a5411aba1112136a6deab155d736c744be7
Sam Endean <samendean@hotmail.com>
2016-06-26 14:12:36 +0100

    oops

---
+++ BrainsEden/Assets/Scripts/Compass/Compass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Compass : MonoBehaviour
+{
+	static Compass m_instance;
+	static public Compass instance { get { return m_instance; } }
+
+	public GameObject compassOrigin;
+
+	void Awake ()
+	{
+		m_instance = this;
+	}
+}
