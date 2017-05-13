63110f74c64a4139cd68d00681f24f57328d0b5a
Ed-McD <edard_666@hotmail.co.uk>
2017-05-13 16:25:08 +0100

    Beginning work on teleport

--- Assets/Scripts/BlockData.cs
+++ Assets/Scripts/BlockData.cs
@@ -2,17 +2,23 @@
 using System.Collections;
 
 [System.Serializable]
-public class BlockData : MonoBehaviour
+public class BlockData : GameActors
 {
     public BlockType blockType;
     public Vector3 localPosition;
     public int ID;
+
+    #region Teleporting Variables
+    public BlockConnection [] connectedBlockIds = new BlockConnection[5];    
+    private BlockData currTargetBlock;
+    #endregion
 
     /// <summary>
     /// initialise the block after being created
     /// </summary>
     public void Initialise()
     {
+        InitDelegates();
         switch (blockType)
         {
             case BlockType.Default:
@@ -28,4 +34,53 @@ public class BlockData : MonoBehaviour
         }
     }
 
+    
+
+    /// <summary>
+    /// Override to recieve the post rotation event;
+    /// </summary>
+    /// <param name="_rotationData"></param>
+    /// <param name="_isInit"></param>
+    protected override void PostRotationLogic(RotationData _rotationData, bool _isInit)
+    {
+        switch (blockType)
+        {
+            
+            case BlockType.Default:
+                break;
+            case BlockType.Teleport:
+                /*currTargetBlock = */ getTeleportTarget(_rotationData.intendedState);
+                break;
+            case BlockType.Moving:
+                break;
+            case BlockType.Falling:
+                break;
+            default:
+                break;
+        }
+        base.PostRotationLogic(_rotationData, _isInit);
+    }
+
+    int getTeleportTarget(CameraState _state)
+    {
+        for (int i = 0; i < connectedBlockIds.Length; i ++)
+        {
+            if (connectedBlockIds[i].cameraView == _state)
+            {
+                return connectedBlockIds[i].connectedBlock;
+            }
+        }
+        return ID;
+    }
+
 }
+
+
+[System.Serializable]
+public class BlockConnection
+{
+    public CameraState cameraView = CameraState.None;
+    public int connectedBlock = -1;
+}
+
+
