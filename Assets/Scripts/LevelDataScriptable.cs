67dc6fbd87ef4e3dcfcc592e3c2f650c4b4ea1a7
Rob Hopps <rob-hopps@hotmail.co.uk>
2017-05-14 12:43:37 +0100

    serialized scriptable object

--- Assets/Scripts/LevelDataScriptable.cs
+++ Assets/Scripts/LevelDataScriptable.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
+[System.Serializable]
 [CreateAssetMenu(fileName = "Data", menuName = "Inventory/List", order = 1)]
 public class LevelDataScriptable : ScriptableObject
 {
