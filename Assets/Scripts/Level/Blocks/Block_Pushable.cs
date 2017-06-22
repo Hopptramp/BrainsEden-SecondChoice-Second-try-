4068a26dd50b06700ccf6735439a75f43b06039b
Ed-McD <edard_666@hotmail.co.uk>
2017-06-22 15:03:23 +0100

    Push Blocks can fall

--- Assets/Scripts/Level/Blocks/Block_Pushable.cs
+++ Assets/Scripts/Level/Blocks/Block_Pushable.cs
@@ -5,6 +5,7 @@ public class Block_Pushable : BlockData {
 
     private Coroutine moving = null;
     public bool isMoving { get { return moving != null; } }
+    LayerMask obstructionObjects;
 
     public override void Initialise()
     {
@@ -18,17 +19,27 @@ public class Block_Pushable : BlockData {
 
     public bool CanBePushed(Vector3 _direction, LayerMask _obstructionObjects)
     {
-        if (Physics.Raycast(transform.position, _direction, 1, _obstructionObjects))
+        if (obstructionObjects != _obstructionObjects)
+            obstructionObjects = _obstructionObjects;
+
+        if (Physics.Raycast(transform.position, _direction, 1, obstructionObjects))
         {
             //Obstruction
             return false;
         }
         //Drop
-        if (!Physics.Raycast(transform.position + _direction, Vector3.down, 1, _obstructionObjects))
-            return false;
+        if (!Physics.Raycast(transform.position + _direction, Vector3.down, 1, obstructionObjects))
+            return true;
 
         //No obstruction
         return true;
+    }
+    public bool CheckObjectBelow()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, 1,obstructionObjects))
+            return true;
+        else
+            return false;          
     }
     
     public void MoveBlock(Vector3 _direction)
@@ -53,7 +64,31 @@ public class Block_Pushable : BlockData {
             t += Time.deltaTime;
         }
         transform.position = newPos;
-        moving = null;
+
+        if (!CheckObjectBelow())        
+            moving = StartCoroutine(Fall(0.2f));
+        else
+            moving = null;
+        yield return null;
+    }
+    IEnumerator Fall(float _duration,  float _overflowTime = 0)
+    {
+        Vector3 origin = transform.position;
+        Vector3 newPos = transform.position + Vector3.down;
+        float t = _overflowTime;
+        
+        while(t< _duration)
+        {
+            transform.position = Vector3.Lerp(origin, newPos, t / _duration);
+            yield return null;
+            t += Time.deltaTime;
+        }
+        transform.position = newPos;
+        if (!CheckObjectBelow())
+            moving = StartCoroutine(Fall(0.2f, t - _duration));
+        else
+            moving = null;
+                
         yield return null;
     }
 
